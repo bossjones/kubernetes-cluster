caa5b43e7638b8fac0426107ea9ec93741704cc8
Malcolm Jones <bossjones@theblacktonystark.com>
2019-01-09 21:59:21 -0500

    wip: elasticsearch

--- elasticsearch/bin/run.sh
+++ elasticsearch/bin/run.sh
@@ -16,6 +16,15 @@
 
 set -ex
 
+# NOTE: Fix issue where mantra hides errors to stderr
+# SOURCE: https://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself
+# Without this, only stdout would be captured - i.e. your
+# log file would not contain any error messages.
+# SEE (and upvote) the answer by Adam Spiers, which keeps STDERR
+# as a separate stream - I did not want to steal from him by simply
+# adding his answer to mine.
+exec 2>&1
+
 export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
